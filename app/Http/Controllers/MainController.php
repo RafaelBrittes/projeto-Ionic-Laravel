15ef8998bf8df7f34b643e140f4412d749d505b5
Rafael Brittes <rafaelbrittes@pop-os.localdomain>
2021-08-15 14:50:47 -0300

    initiation project

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Request;
+
+class MainController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function createUser($request)
+    {
+        //
+    }
+
+    //
+}
---
+++ database/migrations/2021_08_15_165134_create_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 50);
+            $table->integer("CNPJ", 20);
+            $table->string('adress');
+            $table->string('city');
+            $table->string('state');
+            $table->string('phone');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
