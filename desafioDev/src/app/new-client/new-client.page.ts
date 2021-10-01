123ea99dac1c84ed15b390be1c0994ea68ba82c4
Rafael Brittes <rafael.brittes@multipedidos.com.br>
2021-10-01 12:08:16 -0300

    new client done

--- desafioDev/src/app/new-client/new-client.page.ts
+++ desafioDev/src/app/new-client/new-client.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import{ Location } from '@angular/common';
 
 
 @Component({
@@ -13,7 +16,7 @@ export class NewClientPage implements OnInit {
 clientsForm: FormGroup
 
 
-  constructor( private router: Router, private formBuilder: FormBuilder) { 
+  constructor( private router: Router, private formBuilder: FormBuilder, private http: HttpClient, private _location: Location) { 
     this.clientsForm = this.formBuilder.group({
       name: this.formBuilder.control('', [Validators.required, Validators.minLength(4)]),
       cnpj: this.formBuilder.control('', [Validators.required, Validators.minLength(18)]),
@@ -24,10 +27,32 @@ clientsForm: FormGroup
     })
   }
   public url: string = "";
+  mainUrl = 'http://projeto-ionic.beta';
+  
+  // public onSubmit() Observable<any> {
+  //   // console.log(this.clientsForm.value);
+  //   return this.http.post(`${this.mainUrl}/users`, data)
+  // }
+  
+  onSubmit() {
+    var formData: any = new FormData();
+    formData.append("name", this.clientsForm.get('name').value);
+    formData.append("cnpj", this.clientsForm.get('cnpj').value);
+    formData.append("address", this.clientsForm.get('address').value);
+    formData.append("city", this.clientsForm.get('city').value);
+    formData.append("state", this.clientsForm.get('state').value);
+    formData.append("phone", this.clientsForm.get('phone').value);
 
   
-  public onSubmit() {
-    console.log(this.clientsForm.value);
+    this.http.post(`${this.mainUrl}/users`, formData).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
+    this.backToClients();
+  }
+
+  backToClients() {
+    this._location.back();
   }
   
   ngOnInit() {
